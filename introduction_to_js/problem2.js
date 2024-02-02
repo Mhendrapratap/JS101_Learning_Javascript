5eb715f4ff259a7d4d41d96e3acddaa59575ac12
Mhendrapratap <mahendrapratp354@gmail.com>
2024-02-02 09:15:12 +0530

    pushing introduction to javascript problems

---
+++ introduction_to_js/problem2.js
@@ -0,0 +1,18 @@
+/* Define a variable called name with your Name as the assigned value
+Print the value stored in the variable name
+Change the variable to store your father's name
+Print the value stored in the variable name
+Change the variable again to store your mother's name.
+Print the value stored in the variable name */
+
+let name = "Mahendra";
+
+console.log(name);
+
+name = "Narendra Bahadur Singh";
+
+console.log(name);
+
+name = "Vitan";
+
+console.log(name);
---
+++ introduction_to_js/problem3.js
@@ -0,0 +1,15 @@
+/* Create a variable name to store your name
+Create a variable age to store your age
+Print the values stored in the variable on one line followed by the type of the variable in the next line */
+
+let name = "Mahendra";
+
+let age = 27;
+
+console.log(name);
+
+console.log(typeof(name));
+
+console.log(age);
+
+console.log(typeof(age));
